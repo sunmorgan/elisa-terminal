3989c914bfc37a6f27596f62ec58da7ee7084f3a
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2025-07-18 21:50:51 -0400

    Structure objects

---
+++ backend/src/models.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize}
+
+pub struct Quote {
+    pub symbol: String,
+    pub price: String,
+    pub change: String,
+    pub change_percent: String
+}
+
+pub struct AvResponse {
+    pub quote: Option<Quote>
+    pub error: Option<String>
+}
+
+pub struct response {
+    pub symbol: String,
+    pub price: f64,
+    pub change: f64,
+    pub change_percent: String
+}
