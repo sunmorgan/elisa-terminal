83f5787a9f72e41b01c26ecd7261feb34cb0a24c
Morgan Sun <64807003+sunmorgan@users.noreply.github.com>
2025-07-20 17:34:48 -0400

    logging

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,4 +4,16 @@ mod alphavantage;
 mod cache;
 mod models;
 
-async fn main() {}
+use axum:: {
+   extract::Path,
+   routing::get,
+   Router,
+}
+use cache::QuoteCache;
+use models::Response;
+use std::{net::SocketAddr, sync::Arc};
+use tracing_subscriber;
+
+async fn main() {
+    tracing_subscriber::fmt::init();
+}
